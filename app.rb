30aac7824d7c8e2fbbccf6f3b315b25ea669fd53
Gor <7495500@gmail.com>
2021-01-19 08:23:29 +0300

    Initial commit

---
+++ app.rb
@@ -0,0 +1,61 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'sinatra/activerecord'
+require 'sqlite3'
+
+set :database, { adapter: 'sqlite3', database: 'pizzashop.sqlite' }
+
+class Product < ActiveRecord::Base
+end
+
+configure do
+  enable :sessions
+end
+
+helpers do
+  def username
+    session[:identity] ? session[:identity] : 'Hello stranger'
+  end
+end
+
+before '/secure/*' do
+  unless session[:identity]
+    session[:previous_url] = request.path
+    @error = 'Sorry, you need to be logged in to visit ' + request.path
+    halt erb(:login_form)
+  end
+end
+
+get '/' do
+  @products = Product.all
+
+  erb :index
+end
+
+get '/login/form' do
+  erb :login_form
+end
+
+post '/login/attempt' do
+  session[:identity] = params['username']
+  where_user_came_from = session[:previous_url] || '/'
+  redirect to where_user_came_from
+end
+
+get '/logout' do
+  session.delete(:identity)
+  erb "<div class='alert alert-message'>Logged out</div>"
+end
+
+get '/secure/place' do
+  erb 'This is a secret place that only <%=session[:identity]%> has access to!'
+end
+
+get '/about' do
+  erb :about
+end
+
+post '/cart' do
+  erb 'Hello'
+end
